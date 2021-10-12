994f33df50740520044c14ff1a34e436127fd517
Romain Bohdanowicz <romain.bohdanowicz@gmail.com>
2021-10-12 15:50:05 +0200

    Correction

--- Langage/ex-jeu.js
+++ Langage/ex-jeu.js
@@ -1,3 +1,26 @@
+function getRandom() {
+  return Math.random();
+}
+
+function getRandomArbitrary(min, max) {
+  return Math.random() * (max - min) + min;
+}
+
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+
+function getRandomIntInclusive(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min +1)) + min;
+}
+
+
+
+
 // importer l'API readline de Node.js
 const readline = require('readline');
 
@@ -11,13 +34,38 @@ const rl = readline.createInterface({
 
 
 function jouer() {
+  if (essais.length) {
+    console.log('Vous avez déjà joué : ' + essais.join(' - '))
+  }
   rl.question('Quel est le nombre ? ', (answer) => {
     console.log('Vous avez saisi : ' + answer);
-    jouer();
+
+    const entierSaisi = parseInt(answer, 10);
+
+    if (isNaN(entierSaisi)) {
+      console.log('Il faut saisir un entier');
+      return jouer();
+    }
+
+    essais.push(entierSaisi);
+
+    if (entierSaisi > entierAlea) {
+      console.log('Trop grand');
+      jouer();
+    } else if (entierSaisi < entierAlea) {
+      console.log('Trop petit');
+      jouer();
+    } else {
+      console.log('Gagné');
+      rl.close();
+    }
+
   });
 
 }
 
+const entierAlea = getRandomInt(0, 100);
+const essais = [];
 jouer();
 
 // ^
